90812cba87aa4f3ba1d2ed78f4e34b10a7154f29
Gleb Zolotukhin <gazolotukhin@edu.hse.ru>
2025-04-08 03:01:46 +0300

    initial commit

--- Assets/Scripts/ObjectScripts/KnifeUsable.cs
+++ Assets/Scripts/ObjectScripts/KnifeUsable.cs
@@ -1,17 +1,36 @@
-using PlayerScripts;
-using Unity.Netcode;
 using UnityEngine;
+using Unity.Netcode;
+using System.Linq;
+using PlayerScripts;
 
 namespace ObjectScripts
 {
     public class KnifeUsable : Weapon
     {
-        private int _count = 3;
+        private float _range = 1.5f;
         public override float Damage { get; protected set; } = 20f;
+
         protected override void UseFunctional()
         {
-            Debug.Log($"Deal damage to near player: {Damage}");
-            // Прописать анимацию ножа
+            if (IsOwner)
+                TryHitServerRpc();
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        private void TryHitServerRpc()
+        {
+            var hits = Physics.OverlapSphere(transform.position + transform.forward * 0.8f, _range);
+            var nearestPlayer = hits
+                .Select(h => h.GetComponent<PlayerHealth>()) 
+                .Where(p => p != null && p.GetComponent<NetworkObject>().OwnerClientId != OwnerClientId)
+                .OrderBy(p => Vector3.Distance(transform.position, p.transform.position))
+                .FirstOrDefault();
+
+            if (nearestPlayer != null)
+            {
+                nearestPlayer.DamageHealth(Damage);
+                Debug.Log($"Nearest player got {Damage} damage, he has {nearestPlayer.Health} health");
+            }
         }
     }
 }
